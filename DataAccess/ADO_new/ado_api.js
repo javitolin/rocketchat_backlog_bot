29f3922c9a559571dc9f026811218669a744259a
Javier Dorfsman <javitolin@gmail.com>
2023-03-17 12:18:39 +0200

    Move to directories

---
+++ DataAccess/ADO_new/ado_api.js
@@ -0,0 +1,37 @@
+const config = require('config');
+const axios = require("axios")
+
+const ADO_URL = encodeURI(config.get("ado.url"));
+const ADO_PAT = config.get("ado.pat")
+
+// ADO_URL (Should be https://{instance}/{collection}/{project}/_apis)
+
+if (!ADO_URL || !ADO_PAT) {
+  console.error("Missing required environment variables for ADO.");
+  process.exit(1);
+}
+
+const ENCODED_PAT = Buffer.from(`:${ADO_PAT}`).toString('base64');
+
+const adoClient = axios.create({
+  baseURL: ADO_URL,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json-patch+json',
+    "Authorization": `Basic ${ENCODED_PAT}`
+  }
+});
+
+adoClient.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    let res = error?.response;
+    console.error(error?.message)
+    console.error(`Looks like there was a problem. Status Code: ${res?.status}`);
+    return Promise.reject(error);
+  }
+);
+
+module.exports = { adoClient };
---
+++ DataAccess/ADO_new/ticket_api.js
@@ -0,0 +1,77 @@
+const adoClient = require("./ado_api")
+const configuration = require('config');
+const config = require('config');
+
+const ADO_URL = configuration.get("ado.url");
+const ADO_AREA_PATH = config.get("ado.task.area_path")
+
+async function OpenTicket(title, description, dod_content, parent_task_id, tags, assignee) {
+
+    var data = [{
+        "op": "add",
+        "path": "/fields/System.Title",
+        "from": null,
+        "value": title
+    },
+    {
+        "op": "add",
+        "path": "/fields/System.Description",
+        "from": null,
+        "value": description
+    },
+    {
+        "op": "add",
+        "path": "/fields/System.AreaPath",
+        "from": null,
+        "value": ADO_AREA_PATH
+    },
+    {
+        "op": "add",
+        "path": "/fields/Microsoft.VSTS.Common.AcceptanceCriteria",
+        "from": null,
+        "value": dod_content
+    },
+    {
+        "op": "add",
+        "path": "/relations/-",
+        "value": {
+            "rel": "System.LinkTypes.Hierarchy-Reverse",
+            "url": encodeURI(`${ADO_URL}/wit/workitems/${parent_task_id}`)
+        }
+    }];
+
+    if (assignee) {
+        data.push({
+            "op": "add",
+            "path": "/fields/System.AssignedTo",
+            "value": assignee,
+            "from": null
+        })
+    }
+
+    if (tags) {
+        data.push({
+            "op": "add",
+            "path": "/fields/System.Tags",
+            "value": tags
+        })
+    }
+
+    var config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: configuration.get("ado.task.task_url"),
+        data: data
+    };
+
+    try {
+        var response = await adoClient.adoClient(config);
+        return response.data;
+    }
+    catch (err) {
+        console.log("Error running adoClient", err);
+        return null;
+    }
+}
+
+module.exports = { OpenTicket }
